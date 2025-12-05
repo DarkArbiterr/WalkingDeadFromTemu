3fb13e6bdc07419cb518a79e6be91755bbed9d87
Patryk Zając <75552617+DarkArbiterr@users.noreply.github.com>
2025-12-05 20:52:55 +0100

    add Flee behavior

--- steeringBehaviors/steering_behaviors.py
+++ steeringBehaviors/steering_behaviors.py
@@ -2,15 +2,10 @@ import pygame
 
 class SteeringBehaviors:
     def __init__(self, agent):
-        """
-        agent: obiekt
-        """
         self.agent = agent
 
     def seek(self, target_pos):
-        """
-        Seek: zwraca siłę skierowaną w stronę celu
-        """
+        """Seek target position"""
         if target_pos is None:
             return pygame.Vector2(0, 0)
 
@@ -23,4 +18,11 @@ class SteeringBehaviors:
         steering_force = desired_velocity - self.agent.velocity
         return steering_force
 
-    # tu później dodamy inne steering behaviors (flee, arrive, wander itd.)
+    def flee(self, target_pos, panic_distance=None):
+        """Flee from target position. Optional panic distance."""
+        to_target = self.agent.pos - target_pos
+        # jeśli jest panic_distance i cel jest dalej niż dystans paniki, nic nie rób
+        if panic_distance is not None and to_target.length_squared() > panic_distance**2:
+            return pygame.Vector2(0, 0)
+        desired_velocity = to_target.normalize() * self.agent.max_speed
+        return desired_velocity - self.agent.velocity
